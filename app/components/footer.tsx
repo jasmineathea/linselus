1fa093cbca04c4542d5f1bed1157150525326359
jasmineathea <jna009@uib.no>
2025-04-04 22:40:28 +0200

    Footer update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,13 +4,13 @@ export function Footer() {
     return (
       <nav className="flex flex-col items-center gap-3">
         <div className="flex gap-5">
-          <Link className="text-stone-400 hover:text-pink-300 hover:underline" href="https://github.com/jasmineathea">
+          <Link className="text-stone-400 hover:text-pink-100 hover:underline" href="https://github.com/jasmineathea" target="_blank">
             /github
           </Link>
-          <Link className="text-stone-400 hover:text-pink-300 hover:underline" href="https://www.linkedin.com/in/jasmineathea/">
+          <Link className="text-stone-400 hover:text-pink-100 hover:underline" href="https://www.linkedin.com/in/jasmineathea/" target="_blank">
             /linkedin
           </Link>
-          <Link className="text-stone-400 hover:text-pink-300 hover:underline" href="https://www.jasmineathea.no/">
+          <Link className="text-stone-400 hover:text-pink-100 hover:underline" href="https://www.jasmineathea.no/" target="_blank">
             /portfolio
           </Link>
         </div>
